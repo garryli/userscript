ef1eae94476cceabbaec45eaf8f9216be30b47cc
garylirocks <gary.li.rocks@gmail.com>
2017-12-14 10:22:31 +1300

    update code and description, make the main link jump to original as well

--- pocket-goto-original.user.js
+++ pocket-goto-original.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Pocket-goto-Original
-// @namespace   li.garry
-// @description Opens the original instead of the pocket View!
+// @namespace   garyli.rocks
+// @description Opens the original link instead of the pocket View! (please use Ctrl+click)
 // @include     http://getpocket.com/*
 // @include     https://getpocket.com/*
 // @version     1
@@ -10,24 +10,30 @@
 (function() {
     'use strict';
 
-    $(document).ready(function(){        
+    $(document).ready(function(){
+        // add a new style
         var style = $('<style> .original_new { font-weight: bold; color: green !important; } .original_new:hover {font-size: 1.2em !important; } </style>');
         $('html > head').append(style);
         
         $(document).on('mouseover', '.item_content', function() {
             var $this = $(this);
             
+            // check whether already done
             if ($this.hasClass('original_converted')) {return;}
             
+            // get the original url
             var $originalLink = $this.find('.original_url');
-            
             var url = $originalLink.attr('href');
             var originalUrl = decodeURIComponent(url.replace("https://getpocket.com/redirect?url=", ""));
             originalUrl = originalUrl.replace(/&formCheck=.*$/, '');
             console.log(originalUrl);
             
-            $originalLink.attr('href', originalUrl);
-            $originalLink.addClass('original_new');
+            // update link href and styles
+            $originalLink.attr('href', originalUrl).addClass('original_new');
+            $this.find('a.item_link').attr('href', originalUrl);
+            $this.find('a.title').attr('href', originalUrl).addClass('original_new');
+
+            // mark this one as done
             $this.addClass('original_converted');
         });
     });
