9c040e3700e51ddfaf1490023579dc3f92aec4ad
Gary Li <gary@proworkflow.com>
2021-02-11 22:42:20 +1300

    Update pocket-goto-original script for new Pocket UI

--- pocket-goto-original.user.js
+++ pocket-goto-original.user.js
@@ -2,39 +2,105 @@
 // @name        Pocket-goto-Original
 // @namespace   garyli.rocks
 // @description Opens the original link instead of the pocket View! (please use Ctrl+click)
-// @include     http://getpocket.com/*
-// @include     https://getpocket.com/*
-// @version     1.1
+// @include     http://getpocket.com/my-list*
+// @include     https://getpocket.com/my-list*
+// @version     2.0
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function (window) {
+  'use strict';
 
-    $(document).ready(function(){
-        // add a new style
-        var style = $('<style> .original_new { font-weight: bold; color: green !important; } .original_new:hover {font-size: 1.2em !important; } </style>');
-        $('html > head').append(style);
-        
-        $(document).on('mouseover', '.item_content', function() {
-            var $this = $(this);
-            
-            // check whether already done
-            if ($this.hasClass('original_converted')) {return;}
-            
-            // get the original url
-            var $originalLink = $this.find('.original_url');
-            var url = $originalLink.attr('href');
-            var originalUrl = decodeURIComponent(url.replace("https://getpocket.com/redirect?url=", ""));
-            originalUrl = originalUrl.replace(/&formCheck=.*$/, '');
-            console.log(originalUrl);
-            
-            // update link href and styles
-            $originalLink.attr('href', originalUrl).addClass('original_new');
-            $this.find('a.item_link').attr('href', originalUrl);
-            $this.find('a.title').attr('href', originalUrl).addClass('original_new');
+  // add a new class for original link
+  const customStyle = document.createElement('style');
+  customStyle.innerText = `
+    a.__original_link {
+        text-decoration: none;
+    }
 
-            // mark this one as done
-            $this.addClass('original_converted');
-        });
-    });
-})();
+    a.__original_link:hover {
+      text-decoration: none !important;
+    }
+
+    a.__original_link h2::before {
+      margin-right: .2em;
+      padding: .2em;
+      padding-left: 0;
+      content: '○';
+      position: relative;
+      top: -1px;
+    }
+
+    a.__original_link:hover h2::before {
+      content: '●';
+    }
+  `;
+  document.querySelector('head').appendChild(customStyle);
+
+  function addOriginalLinks() {
+    console.log('Add original links');
+
+    const allItems = window.__NEXT_REDUX_WRAPPER_STORE__.getState()
+      .myListItemsById;
+
+    // wrap title in an <a> element with original url
+    document
+      .querySelectorAll('article[data-cy^=article-card-]')
+      .forEach(function (e) {
+        if (e.dataset['original_link_added']) {
+          return;
+        }
+
+        const itemId = e.dataset['cy'].replace(/article-card-(\d+)/, '$1');
+
+        if (allItems[itemId]) {
+          const originalUrl = allItems[itemId]['save_url'];
+          const originalLink = document.createElement('a');
+          originalLink.href = originalUrl;
+          originalLink.classList.add('__original_link');
+          originalLink.append(e.querySelector('h2'));
+          e.querySelector('.content').prepend(originalLink);
+        }
+
+        e.dataset['original_link_added'] = 'true';
+      });
+  }
+
+  let prevList;
+
+  const getListNode = () => {
+    return document.querySelector('.main header + div');
+  };
+
+  const config = { subtree: true, childList: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+      addOriginalLinks();
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  const checkList = setInterval(function () {
+    const newList = getListNode();
+    if (newList !== prevList) {
+      if (observer) {
+        observer.disconnect();
+      }
+
+      if (newList) {
+        console.log('A new list loaded');
+        prevList = newList;
+        addOriginalLinks();
+
+        // Start observing the target node for configured mutations
+        observer.observe(prevList, config);
+      } else {
+        console.log('waiting');
+      }
+    }
+  }, 1000);
+})(window.unsafeWindow);
+
